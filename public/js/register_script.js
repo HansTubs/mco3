379f3bee3e2804d2339ca800fda2df8a9607b4a2
HansTubs <117156633+HansTubs@users.noreply.github.com>
2023-08-06 14:59:18 +0800

    Add files via upload

---
+++ public/js/register_script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const wrapper = document.querySelector(".wrapper");
+    const signupLink = document.querySelector(".signup-link");
+    const loginLink = document.querySelector(".login-link");
+  
+    signupLink.addEventListener('click', () => {
+      wrapper.classList.add('active');
+    });
+  
+    loginLink.addEventListener('click', () => {
+      wrapper.classList.remove('active');
+    });
+  });
